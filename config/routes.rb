10cc8a514c3532015d25178160e62aa3712beeb4
fu3nori <cap.fuminori.usui@gmail.com>
2025-04-05 19:22:42 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  # 例: ホームページ
+  # ホーム画面の表示
   get "home/index"
   root "home#index"
 
-  # ヘルスチェック（ロードバランサ等用）
+  # ヘルスチェック (ロードバランサー等用)
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # PWA 用
+  # PWA 用ファイルのルーティング
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest"        => "rails/pwa#manifest",        as: :pwa_manifest
 end
+
 
 
